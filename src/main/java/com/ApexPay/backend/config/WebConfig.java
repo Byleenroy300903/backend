0070ebfb1b75178c89ed7653280932f3828f6068
Byleen Janet Roy J <113660030+Byleenroy300903@users.noreply.github.com>
2026-02-16 19:20:47 +0530

    Update WebConfig.java

--- src/main/java/com/ApexPay/backend/config/WebConfig.java
+++ src/main/java/com/ApexPay/backend/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**") // Allow all API endpoints
-                        .allowedOrigins("http://localhost:4200") // Your Angular URL
+                        .allowedOrigins("*") // Your Angular URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
